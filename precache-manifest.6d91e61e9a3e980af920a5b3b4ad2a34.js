1a9f53c900fd9a9087ff0ded8efbf6fc64553f40
Dmitrij Prokhorenko <dmitrij.prokhorenko@gmail.com>
2020-07-20 22:47:35 +0300

    Updates

---
+++ precache-manifest.6d91e61e9a3e980af920a5b3b4ad2a34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caae7ffba7b0c2441c1514d6eb4406e3",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "5f2c763ccafec0cc9e13",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.fd343404.chunk.css"
+  },
+  {
+    "revision": "1d0bca3ce77d93110868",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b2a72b8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b2a72b8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2c763ccafec0cc9e13",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.90ab2af1.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
